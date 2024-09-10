81c64c0d6d62ae39501895292010127af4694153
Dipak Sarkar <hello@dipaksarkar.in>
2024-09-10 22:47:34 +0530

    Remove Layer Panel

--- src/panels.ts
+++ src/panels.ts
@@ -1,7 +1,7 @@
 import type { Editor } from 'grapesjs'
 import { RequiredPluginOptions } from '.'
 
-import { cmdImport, cmdClear } from './consts'
+import { cmdImport } from './consts'
 
 export default (editor: Editor, opts: RequiredPluginOptions) => {
   const { Panels } = editor
@@ -10,7 +10,6 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
   const expt = 'export-template'
   const osm = 'open-sm'
   const otm = 'open-tm'
-  const ola = 'open-layers'
   const obl = 'open-blocks'
   const ful = 'fullscreen'
   const prv = 'preview'
@@ -78,12 +77,7 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
         {
           id: otm,
           command: otm,
-          label: `<svg ${iconStyle} viewBox="0 0 24 24"><path fill="currentColor" d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" /></svg>`
-        },
-        {
-          id: ola,
-          command: ola,
-          label: `<svg ${iconStyle} viewBox="0 0 24 24"><path fill="currentColor" d="M12,16L19.36,10.27L21,9L12,2L3,9L4.63,10.27M12,18.54L4.62,12.81L3,14.07L12,21.07L21,14.07L19.37,12.8L12,18.54Z" /></svg>`
+          label: `<svg ${iconStyle} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z"/></svg>`
         },
         {
           id: obl,
@@ -101,14 +95,10 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
   opts.showStylesOnChange &&
     editor.on('component:selected', () => {
       const openSmBtn = Panels.getButton('views', osm)
-      const openLayersBtn = Panels.getButton('views', ola)
 
       // Don't switch when the Layer Manager is on or
       // there is no selected component
-      if (
-        (!openLayersBtn || !openLayersBtn.get('active')) &&
-        editor.getSelected()
-      ) {
+      if (editor.getSelected()) {
         openSmBtn?.set('active', true)
       }
     })
